5071b1c147c0c55b4dae2ed6fcb814244375ccbf
vineethmovva9 <156262004+vineethmovva9@users.noreply.github.com>
2025-12-09 19:54:12 -0500

    test

--- MoviesSystem.js
+++ MoviesSystem.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const portNumber = 7003;
+const portNumber = process.env.PORT || 7003;
 const bodyParser = require("body-parser");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
